4b8634cc9967af852fdded87226df569940558fc
Hussein-Mansour <74609841+Hussein-Mansour@users.noreply.github.com>
2021-04-26 23:49:07 -0400

    Update volume_math.cpp

--- volume_math.cpp
+++ volume_math.cpp
@@ -7,23 +7,22 @@
 
 #include <iostream>
 
-int main()
-{
+int main(){
 	// variable declarations
-    int length;
-    int width;
-    int height;
-    int volume;
-    // input
-    std::cout << "We will calculate the volume of a rectangular prism (cm):" << std::endl;
-    std::cout << "Enter the length (cm): ";
-    std::cin >> length;
-    std::cout << "Enter the width (cm): ";
-    std::cin >> width;
-    std::cout << "Enter the height (cm):";
-    std::cin >> height;
-    // process
-    volume = length*width*height;
-    // output
-    std::cout << "Area is " << (volume) << "mm2" << std::endl;
+	int length;
+	int width;
+	int height;
+	int volume;
+	// input
+	std::cout << "We will calculate the volume of a rectangular prism (cm):" << std::endl;
+	std::cout << "Enter the length (cm): ";
+	std::cin >> length;
+	std::cout << "Enter the width (cm): ";
+	std::cin >> width;
+	std::cout << "Enter the height (cm):";
+	std::cin >> height;
+	// process
+	volume = length*width*height;
+	// output
+	std::cout << "Area is " << (volume) << "cm3" << std::endl;
 }
